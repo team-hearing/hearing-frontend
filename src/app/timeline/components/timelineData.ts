00c42ff3d2b21c93dd69aa0fc45d288b291982ff
Hnaji <agneskim92@naver.com>
2025-05-03 18:52:48 +0900

    style: 상세페이지 레이아웃 수정중

--- src/app/timeline/components/timelineData.ts
+++ src/app/timeline/components/timelineData.ts
@@ -4,25 +4,25 @@ export const timelineData: TimelineData[] = [
   {
     year: 2025,
     events: [
-      { date: "25.00.00", title: "사건 테스트" },
-      { date: "", title: "사건2" },
-      { date: "", title: "사건3" }
+      { id: 1, date: "25.00.00", title: "사건 테스트" },
+      { id: 2, date: "", title: "사건2" },
+      { id: 3, date: "", title: "사건3" }
     ]
   },
   {
     year: 2024,
     events: [
-      { date: "24.05.15", title: "사건 테스트" },
-      { date: "24.08.20", title: "사건2" },
-      { date: "24.12.25", title: "사건3" }
+      { id: 4, date: "24.05.15", title: "사건 테스트" },
+      { id: 5, date: "24.08.20", title: "사건2" },
+      { id: 6, date: "24.12.25", title: "사건3" }
     ]
   },
   {
     year: 2023,
     events: [
-      { date: "23.02.10", title: "사건 테스트" },
-      { date: "23.06.30", title: "사건2" },
-      { date: "23.11.11", title: "사건3" }
+      { id: 7, date: "23.02.10", title: "사건 테스트" },
+      { id: 8, date: "23.06.30", title: "사건2" },
+      { id: 9, date: "23.11.11", title: "사건3" }
     ]
   }
 ]; 
