9ff834fd22e9d76f225cf662214a71dc77f29764
Hnaji <agneskim92@naver.com>
2025-06-07 16:58:51 +0900

    style: font size 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,7 @@ export default function LottieIntroPage() {
             className="lottie-loading"
           />
         ) : (
-          <div className="text-white text-lg">Hearin...</div>
+          <div className="text-white text-body sm:text-h6 md:text-h5 lg:text-h4 font-kr font-regular">Hearing...</div>
         )}
       </div>
     );
