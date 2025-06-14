5bbf39c78f229e83606ab9e69a8d1019796f9339
Hnaji <agneskim92@naver.com>
2025-06-14 15:36:41 +0900

    style: 영상 1.5 재생 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function IntroPage() {
 
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.playbackRate = 1.0;
+      videoRef.current.playbackRate = 1.5;
     }
   }, []);
 
